7b8caa426f295b2fa7c9973947f43b9bb3e2041a
Mohammad Ahmad <>
2018-11-28 19:10:31 -0600

    ...

--- nimbus.py
+++ nimbus.py
@@ -197,11 +197,11 @@ class Nimbus(object):
 		#Find spout IP to send to everybody so they can send ACKs to spout
 		spout_ip = self.reverse_mapping[spout_node]
 		print self.failure_detector_node.master_id
-		
+
 		for worker in self.config:
 			self.config[worker]['spout_ip_port'] = (spout_ip[0], SPOUT_LISTEN_PORT)
 			self.config[worker]['client_ip_port'] = (addr[0], CLIENT_LISTEN_PORT)
-			self.config[worker]['file_system_master'] = self.failure_detector_node.master_id
+			self.config[worker]['file_system_master'] = self.failure_detector_node.master_id[0]
 		
 			if self.config[worker]['type'] == 'bolt':
 				self.config[worker]['listen_port'] = self.reverse_mapping[worker][1]
