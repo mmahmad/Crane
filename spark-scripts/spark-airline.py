391349b0546ff873e986fafc6880c3e48abe3f0a
Mohammad Ahmad <>
2018-12-01 15:18:24 -0600

    Change APP_NAME

---
+++ spark-scripts/spark-airline.py
@@ -0,0 +1,34 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode
+from pyspark.sql.functions import split
+from pyspark.sql.types import StructType
+from pyspark.sql import SQLContext
+
+spark = SparkSession \
+    .builder \
+    .appName("spark_airline") \
+    .master("spark://172.22.158.8:7077") \
+    .getOrCreate()
+
+# logData = spark.read.text('/home/mmahmad3/cs425mp4/input/tweets-5k.csv').cache()
+
+# df = spark.read.option("header", "false").csv("/home/mmahmad3/cs425mp4/input/tweets-5k-with-header.csv")
+df = spark.read.csv("/home/mmahmad3/cs425mp4/input/airline-5k-with-header.csv")
+df.cache()
+# df.show()
+# milCyDf = df.loc[df['username'] == '@mileycyrus']
+
+# newDf = df.filter(df['_c4'] == '2Hood4Hollywood').show()
+
+df.createOrReplaceTempView("airlines")
+sqlDF = spark.sql("SELECT count(*) FROM airlines WHERE _c16='LAX' and _c14 > 0")
+sqlDF.show()
+
+
+
+# numAs = logData.filter(logData.value.contains('@mileycyrus')).count()
+
+# print("Lines with @mileycyrus: %i" % (numAs))
+# numAs.pprint()
+
+spark.stop()
