64db56758052dc052397f88896618b3ba28d1ea7
Mohammad Ahmad <>
2018-12-01 16:30:28 -0600

    Set logLevel to WARN

--- spark-scripts/spark-airline.py
+++ spark-scripts/spark-airline.py
@@ -10,6 +10,8 @@ spark = SparkSession \
     .master("spark://172.22.156.10:7077") \
     .getOrCreate()
 
+spark.setLogLevel("WARN")
+
 df = spark.read.csv("/home/mmahmad3/cs425mp4/input/airline-5k-with-header.csv")
 df.cache()
 
